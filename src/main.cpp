ff38fc2f45216908f44c8323f575828dcb812647
bit <dargon-home@qq.com>
2020-10-06 23:29:13 +0800

    style(main.cpp): 参数报错格式修改

--- src/main.cpp
+++ src/main.cpp
@@ -7,31 +7,36 @@ const int limit_arg = 100000;
 
 int main(int argc, char* argv[]) {
 	srand(time(0));
-	if (argc != 5) cout << "argc error!\n";
+	if (argc != 5) {
+		cout << "argc error!\n"
+			<< "correct arguments mode:\n"
+			<< "1. project.exe -n [0,100000) -r [0,100000)\n"
+			<< "2. project.exe -e <exercisefile>.txt -a <answerfile>.txt\n";
+	}
 	else if (argv[1][1] == 'n' && argv[3][1] == 'r') {
 		int a = 0, b = 0, i;
 		i = 0;
 		while ('0' <= argv[2][i] && argv[2][i] <= '9') {
 			a = a * 10 + argv[2][i] - '0'; i++;
 			if (a > limit_arg) {
-				cout << "arg too large!\n";
+				cout << "arg too large! argument number2 should between 0 and 100000 but 100000.\n";
 				return 0;
 			}
 		}
 		if (argv[2][i]) {
-			cout << "arg error!\n";
+			cout << "arg error! argument number2 should be pure number.\n";
 			return 0;
 		}
 		i = 0;
 		while ('0' <= argv[4][i] && argv[4][i] <= '9') {
 			b = b * 10 + argv[4][i] - '0'; i++;
 			if (b > limit_arg) {
-				cout << "arg too large!\n";
+				cout << "arg too large! argument number4 should between 0 and 100000 but 100000.\n";
 				return 0;
 			}
 		}
 		if (argv[4][i]) {
-			cout << "arg error!\n";
+			cout << "arg error! argument number4 should be pure number.\n";
 			return 0;
 		}
 		run_gene(a, b - 1);
@@ -42,7 +47,12 @@ int main(int argc, char* argv[]) {
 			cout << "adress error!\n";
 		else sana_test(exe, ans);
 	}
-	else cout << "arg model error!\n";
+	else {
+		cout << "mode error!\n"
+			<< "correct arguments mode:\n"
+			<< "1. project.exe -n [0,100000) -r [0,100000)\n"
+			<< "2. project.exe -e <exercisefile>.txt -a <answerfile>.txt\n";
+	}
 	
 	return 0;
 }
